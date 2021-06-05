a7a97967bf61c878f7811fe8877889939962b1ee
Itzkartik <smartzboi@gmail.com>
2021-06-05 10:18:45 +0530

    work

--- eng/m.js
+++ eng/m.js
@@ -1,5 +1,5 @@
 import {Stripe} from 'https://js.stripe.com/v3/';
-function runme() {
+export function runme() {
     var stripe = Stripe('pk_test_51Iy7TESHTROt2vsbvjWmIOZEvJjBJ2JX36XTg6MywV644hXwLADs0ISsjgNTKicWsqAkTtZk3Hn4czavyQ8hLu5W00vD2ZjQG4', {
         apiVersion: "2020-08-27",
     });
