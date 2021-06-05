414bb5d9bc9dcbea8508072375ae438b4a1f9368
Itzkartik <smartzboi@gmail.com>
2021-06-05 11:11:17 +0530

    work

--- eng/m.js
+++ eng/m.js
@@ -1,4 +1,32 @@
-function runme() {
+function initPayPalButton() {
+    paypal.Buttons({
+    style: {
+        shape: 'rect',
+        color: 'gold',
+        layout: 'vertical',
+        label: 'paypal',
+        
+    },
+
+    createOrder: function(data, actions) {
+        return actions.order.create({
+        purchase_units: [{"amount":{"currency_code":"USD","value":1}}]
+        });
+    },
+
+    onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+        alert('Transaction completed by ' + details.payer.name.given_name + '!');
+        });
+    },
+
+    onError: function(err) {
+        console.log(err);
+    }
+    }).render('#paypal-button-container');
+}
+
+function stripe_payment() {
     var stripe = parent.Stripe('pk_test_51Iy7TESHTROt2vsbvjWmIOZEvJjBJ2JX36XTg6MywV644hXwLADs0ISsjgNTKicWsqAkTtZk3Hn4czavyQ8hLu5W00vD2ZjQG4', {
         apiVersion: "2020-08-27",
     });
