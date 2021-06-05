b9e40f40e4debec8966945e80330ffc8e8d5f6f3
Itzkartik <smartzboi@gmail.com>
2021-06-05 10:48:50 +0530

    work

--- eng/m.js
+++ eng/m.js
@@ -1,6 +1,7 @@
 function runme(ele) {
-    var ele = $(ele).parent('payment-div')
-    console.log(ele.prevObject[0].parentNode.childNodes[2].value);
+    var ele = $(ele).parent('payment-div');
+    amt = ele.prevObject[0].parentNode.childNodes[2].value;
+    console.log(amt);
     var stripe = Stripe('pk_test_51Iy7TESHTROt2vsbvjWmIOZEvJjBJ2JX36XTg6MywV644hXwLADs0ISsjgNTKicWsqAkTtZk3Hn4czavyQ8hLu5W00vD2ZjQG4', {
         apiVersion: "2020-08-27",
     });
