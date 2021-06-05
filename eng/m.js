cf5c574eda55634c8b623b2f73fbbefb3eeecc80
Itzkartik <smartzboi@gmail.com>
2021-06-05 10:58:41 +0530

    work

--- eng/m.js
+++ eng/m.js
@@ -1,8 +1,10 @@
 function runme(ele) {
-    var ele = $(ele).parent('payment-div');
-    amt = ele.prevObject[0].parentNode.childNodes[2].value;
-    amt = amt+".00";
-    amt = parseInt(amt);
+    // var ele = $(ele).parent('payment-div');
+    // amt = ele.prevObject[0].parentNode.childNodes[2].value;
+    // amt = amt+".00";
+    // amt = parseInt(amt);
+    amt = $('.payment-div').val();
+    amt = parseFloat(amt+".00");
     var stripe = Stripe('pk_test_51Iy7TESHTROt2vsbvjWmIOZEvJjBJ2JX36XTg6MywV644hXwLADs0ISsjgNTKicWsqAkTtZk3Hn4czavyQ8hLu5W00vD2ZjQG4', {
         apiVersion: "2020-08-27",
     });
@@ -21,14 +23,12 @@ function runme(ele) {
     var prButton = elements.create('paymentRequestButton', {
         paymentRequest: paymentRequest,
     });
-    prButton.mount('#payment-request-button');
 
     paymentRequest.canMakePayment().then(function (result) {
-        var head = $('#tabMake_Payment').contents().find('payment-request-button');
         if (result) {
-            prButton.mount(head);
+            prButton.mount('#payment-request-button');
         } else {
-            head.style.display = 'none';
+            document.getElementById('payment-request-button').style.display = 'none';
         }
     });
 }
